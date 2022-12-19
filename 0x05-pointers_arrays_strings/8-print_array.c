a56a8962bc3a6a36b6cd987bba56d78baf84872c
JOHN ALI <116637847+john12gate@users.noreply.github.com>
2022-12-19 13:22:14 -0800

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
- * puts_half - function that print half of a string
+ * _atoi - convert a string to an integer.
+ * @s: the string to be converted.
  *
- * @str: pointer to char
- * Return: 0 (nothing)
+ * Return: The integer value of the converted string.
  */
 
-void puts_half(char *str)
+int _atoi(char *s)
 {
-int i = 0;
-int n = 0;
+	int sign = 1;
+	unsigned int num = 0;
 
-while (str[i] != '\0')
-	i += 1;
-n = i / 2;
-if (i % 2 == 1)
-	n += 1;
-while (*(str + n) != '\0')
-{
-	_putchar(*(str + n));
-	n++;
-}
-_putchar('\n');
+	do {
+
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
 }
