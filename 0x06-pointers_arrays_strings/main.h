b91e943ba4b076204b7e5a32838fe7b04dab5e12
JOHN ALI <116637847+john12gate@users.noreply.github.com>
2022-12-22 11:02:03 -0800

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *n);
 char *cap_string(char *n);
 char *leet(char *n);
-char *rot13(char *n);
+char *rot13(char *s);
 
 
 #endif /* MAIN_H */
